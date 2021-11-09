7eb24b7bbd29a5f6eb9502ffdc668ad0c106dbea
Lucas Gasque <lucasfdg@hotmail.com>
2021-11-09 11:59:21 -0300

    feature: criado provider de restaurantes e sua tipagem

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,46 @@ export interface UserData {
   emailConfirm?: string;
   passwordConfirm?: string;
 }
+
+export interface RestaurantsData{
+  name: string
+  type: string
+  location: LocationData
+  address: string
+  city: string
+  img: string
+  menu: MenuData
+  businessHours: number
+  id: number
+}
+
+export interface MenuData{
+  entry: DishesData[]
+  main: DishesData[]
+  desert: DishesData[]
+}
+
+export interface DishesData{
+  name: string
+  description: string
+  img: string
+  price: number
+}
+
+export interface LocationData{
+  lng: number
+  ltd: number
+}
+
+export interface ScoreData{
+  id: number
+  userId: number
+  restaurantId: number
+  score: number
+}
+
+export interface FavoritesData{
+  id: number
+  userId: number
+  restaurantId: number
+}
