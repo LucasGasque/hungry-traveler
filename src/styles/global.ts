77de211c1e9f7686b4000977afc6e52468cf5743
Tulio Goulart <tulio.gp2@gmail.com>
2021-11-18 20:46:11 +0100

    fix: ajustado background-color

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,6 +16,12 @@ export default createGlobalStyle`
     border: none;
     outline: none;
     }
+
+    html {
+        @media(min-width: 768px) {
+            background-color: var(--gray-20);
+        }
+    }
     
     body{}
 
